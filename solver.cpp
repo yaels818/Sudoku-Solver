9b0a5f1fea0ee98d58692a6835ee362e38ca3167
Yael <yaelsc.edu@gmail.com>
2023-09-17 16:02:41 +0300

    added find_empty_cell

--- solver.cpp
+++ solver.cpp
@@ -2,6 +2,32 @@
 ------------------------------------------------
 Author: yaels818
 DOC: 17/09/2023
+
+Notes:
+    1. Default board size is 3X3. 
+        You can change it using the SIZE and ROOT_SIZE constant variables
+        and it would work for a 4X4 or any other size you might want.
+    
+    2. User can input any (solvable) Sudoku board, example format is documnented 
+        in the input_into_board function.
+
+The Algorithm:
+    
+    Starting with an incomplete board:
+
+    1.  Find the next empty cell.
+    
+    2.  Attempt to place one of the digits 1-SIZE in that cell.
+    
+    3.  Check if that digit is valid in the current cell based on the current board
+        (check if there are any collisions within the row, col or box).
+
+    4.  a. If the digit is valid, recursively attempt to fill the board using steps 1-3. 
+        b. If it is not valid, reset the cell you just filled and go back to the previous step.
+    
+    5.  Once the board is full, by the definition of this algorithm, 
+        we have found a valid solution (all digits have been checked and found to be valid).
+
 */
 
 #include <iostream>
@@ -108,6 +134,46 @@ void print_board(vector<vector<int>> board){
     }
 }
 
+int* find_empty_cell(vector<vector<int>> *board){
+    /*
+        Find the next empty cell.
+
+        Parameters:
+            vector<vector<int>> *board = pointer to our board
+
+        Return:
+            [i,j] position of the next empty cell (with the digit 0)
+            [-1,-1] if board is full
+
+    */
+
+    // create a new dynamically allocated array [-1,-1] 
+    int* pos = new int[2];
+    pos[0] = -1;
+    pos[1] = -1;
+    
+    // for every row in board
+    for (int i = 0; i < SIZE; i++)
+    {
+        // for every cell in row
+        for (int j = 0; j < SIZE; j++)
+        {
+            // if cell is empty
+            if ((*board)[i][j] == 0)
+            {
+                // store cell position in the array and return it
+                pos[0] = i;
+                pos[1] = j;
+
+                return pos;
+            }
+        }
+    }
+
+    // if no empty cells were found, return [-1,-1]
+    return pos;
+}
+
 int main () {
     // create the sodoku board and a pointer to it
     vector<vector<int>> board(SIZE);
@@ -116,6 +182,13 @@ int main () {
     if (input_into_board(p_board) == 0)
     {
         print_board(board);
+        int* empty_pos = find_empty_cell(p_board);
+        
+        cout << "[" << empty_pos[0] << "] [" << empty_pos[1] << "]" << endl;
+
+        // free dynamically allocated memory
+        free(empty_pos);
+
     }
 
 }
