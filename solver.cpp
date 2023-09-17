a02cbdeb25d8b2513701207749679b8e97c7a415
Yael <yaelsc.edu@gmail.com>
2023-09-17 12:20:09 +0300

    created solver.cpp

---
+++ solver.cpp
@@ -0,0 +1,13 @@
+/*Soduko Solver (using a backtracking algorithm)
+------------------------------------------------
+Author: yaels818
+DOC: 17/09/2023
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+
+}
